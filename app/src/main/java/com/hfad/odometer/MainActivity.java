74cb5e88e0b7949d54d2ac7f507fa42e2dbb1e71
Monir Hossain <mknishad1@ymail.com>
2018-05-24 16:01:12 +0600

    create, bind and unbind service

--- app/src/main/java/com/hfad/odometer/MainActivity.java
+++ app/src/main/java/com/hfad/odometer/MainActivity.java
@@ -1,13 +1,50 @@
 package com.hfad.odometer;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 
 public class MainActivity extends Activity {
+
+    private OdometerService odometer;
+    private boolean bound = false;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            OdometerService.OdometerBinder odometerBinder = (OdometerService.OdometerBinder) service;
+            bound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            bound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, OdometerService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (bound) {
+            unbindService(connection);
+            bound = false;
+        }
     }
 }
