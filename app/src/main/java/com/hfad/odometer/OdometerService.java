45b4fb4b1bf12b7a21407fed5eebf52a3825bea0
Monir Hossain <mknishad1@ymail.com>
2018-05-28 12:58:53 +0600

    Get distance from location service

--- app/src/main/java/com/hfad/odometer/OdometerService.java
+++ app/src/main/java/com/hfad/odometer/OdometerService.java
@@ -1,20 +1,81 @@
 package com.hfad.odometer;
 
+import android.Manifest;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
-
-import java.util.Random;
+import android.support.v4.content.ContextCompat;
 
 public class OdometerService extends Service {
 
     private final IBinder binder = new OdometerBinder();
-    private final Random random = new Random();
+    private LocationListener listener;
+    private LocationManager locManager;
+    public static final String PERMISSION_STRING = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static double distanceInMeters;
+    private static Location lastLocation = null;
 
     public class OdometerBinder extends Binder {
         OdometerService getOdometer() {
             return OdometerService.this;
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (lastLocation == null) {
+                    lastLocation = location;
+                }
+                distanceInMeters += location.distanceTo(lastLocation);
+                lastLocation = location;
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(this, PERMISSION_STRING) == PackageManager.PERMISSION_GRANTED) {
+            String provider = locManager.getBestProvider(new Criteria(), true);
+            if (provider != null) {
+                locManager.requestLocationUpdates(provider, 1000, 1, listener);
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (locManager != null && listener != null) {
+            if (ContextCompat.checkSelfPermission(this, PERMISSION_STRING) == PackageManager.PERMISSION_GRANTED) {
+                locManager.removeUpdates(listener);
+            }
+            locManager = null;
+            listener = null;
         }
     }
 
@@ -24,6 +85,6 @@ public class OdometerService extends Service {
     }
 
     public double getDistance() {
-        return random.nextDouble();
+        return this.distanceInMeters / 1609.344;
     }
 }
